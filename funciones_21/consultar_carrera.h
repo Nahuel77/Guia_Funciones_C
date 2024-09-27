b56fc2d0dfb502d24cb46312800ec0e3d2db67b3
Nahuel <70348604+Nahuel77@users.noreply.github.com>
2024-09-27 04:28:55 -0300

    Add files via upload

--- funciones_21/consultar_carrera.h
+++ funciones_21/consultar_carrera.h
@@ -33,16 +33,41 @@ void consultar_carrera(char atletas[15][100], int t_carrera_a[15], int t_carrera
                 }
             }
             while(status==2);
-        }else{
-            if(carrera==1){
+        }
+        else
+        {
+            if(carrera==1)
+            {
                 mejor_t_carrera(atletas, t_carrera_a);
             }
-            if(carrera==2){
+            if(carrera==2)
+            {
                 mejor_t_carrera(atletas, t_carrera_b);
             }
-            if(carrera==3){
+            if(carrera==3)
+            {
                 mejor_t_carrera(atletas, t_carrera_c);
             }
+            do
+            {
+                printf("Desea consultar otra carrera? S/N: ");
+                scanf(" %c", &op);
+                op = toupper(op);
+                if(op=='N')
+                {
+                    status=0;
+                }
+                if(op=='S')
+                {
+                    status=1;
+                }
+                if(op!='N'&&op!='S')
+                {
+                    printf("La opcion ingresada es incorrecta.\n");
+                    status=2;
+                }
+            }
+            while(status==2);
         }
     }
     while(status!=0);
